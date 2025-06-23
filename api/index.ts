304764a186c88ffafa4dd5b5ad3b1320ffdea140
SabriBere <sabrinab786@gmail.com>
2025-06-23 14:19:39 -0300

    modularizando middleware not found

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import morgan from "morgan"; //combinar con winston o pino para logs de servidor
 import helmet from "helmet";
 import cors from "cors";
 import routes from "./routes/routes";
+import notFound from "./middlewares/notFound";
 
 const server = express();
 server.use(helmet());
@@ -10,12 +11,12 @@ server.use(express.json());
 server.use(cors()); //configuración wild card
 server.use(morgan("dev")); //configuración básica para desarrollo
 
+//configuración de swagger - documentación de end points
+
 server.use("/api", routes);
 
 // middleware de notFound
-server.use((req, res, next) => {
-    res.status(404).json({ message: "Endpoint not found" });
-});
+server.use(notFound);
 
 function startServer() {
     server.listen(process.env.PORT, () => {
