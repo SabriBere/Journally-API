633c85cce849223f9435c68ca6923c64199a9a7a
SabriBere <sabrinab786@gmail.com>
2025-07-07 17:59:07 -0300

    introduciendo validaciones para los campos email, password y usuario

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 const router = Router();
 import UserControllers from "../controllers/usersControllers";
+import { validateUser } from "../middlewares/userValidation";
 
-router.post("/create", UserControllers.create);
+router.post("/register", validateUser, UserControllers.create);
 
 router.post("/login", UserControllers.login);
 
