d8d1c66351180f0405d22346e9496c7992a71dd4
SabriBere <sabrinab786@gmail.com>
2025-07-22 11:24:52 -0300

    end point para consultar todos los post de un usuario (sin paginado ni order by)

--- api/controllers/postControllers.ts
+++ api/controllers/postControllers.ts
@@ -3,8 +3,13 @@ import PostServices from "../services/postServices";
 
 class PostControllers {
     static async allPost(req: Request, res: Response) {
-        const { error, data } = await PostServices.getAllPost();
-        // if (error) return res.status(400).json({ data });
+        const id = Number(req.query.id);
+        const { status, error, data } = await PostServices.getAllPost(id);
+        if (error) {
+            if (status === 404) {
+                return res.status(404).json({ data });
+            }
+        }
         res.status(200).json({ data });
     }
 }
--- api/routes/post.ts
+++ api/routes/post.ts
@@ -2,6 +2,11 @@ import { Router } from "express";
 import postControllers from "../controllers/postControllers";
 const router = Router();
 
+//buscar un post por id
+
+//todos los post de un usuario
 router.get("/", postControllers.allPost);
+
+//editar un post
 
 export default router;
