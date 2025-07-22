3ce20ed538b7448e368d0b4d7988a70918857920
SabriBere <sabrinab786@gmail.com>
2025-07-22 11:24:52 -0300

    end point para consultar todos los post de un usuario (sin paginado ni order by)

--- api/services/postServices.ts
+++ api/services/postServices.ts
@@ -1,7 +1,30 @@
+import prisma from "../db/db";
 class PostServices {
-    static async getAllPost() {
+    static async getAllPost(id: number) {
         try {
-            return { error: false, data: "Hola" };
+            const userExists = await prisma.user.findUnique({
+                where: { user_id: id },
+            });
+
+            if (!userExists) {
+                return {
+                    status: 404,
+                    error: true,
+                    data: "Usuario no encontrado",
+                };
+            }
+
+            const userPost = await prisma.post.findMany({
+                where: {
+                    user_id: id,
+                },
+                include: {
+                    collection: true,
+                    user: true,
+                },
+            });
+
+            return { status: 200, error: false, data: userPost };
         } catch (error: any) {
             return { error: true, data: error.message };
         }
