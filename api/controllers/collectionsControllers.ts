ab993d6b087b1403b9f94d3a782d41bf0d111ea0
SabriBere <sabrinab786@gmail.com>
2025-07-23 21:59:59 -0300

    end point para editar titulo de una colección

--- api/controllers/collectionsControllers.ts
+++ api/controllers/collectionsControllers.ts
@@ -16,6 +16,21 @@ class CollectionsControllers {
 
         res.status(201).json({ data });
     }
+
+    static async updateName(req: Request, res: Response) {
+        const { status, error, data } = await ColletionServices.update(
+            req.body
+        );
+
+        if (error) {
+            if (status === 404) {
+                return res.status(400).json({ data });
+            }
+            return res.status(500).json({ data });
+        }
+
+        res.status(200).json({ data });
+    }
 }
 
 export default CollectionsControllers;
--- api/routes/colletions.ts
+++ api/routes/colletions.ts
@@ -6,9 +6,10 @@ const router = Router()
 //crear una colección
 router.post("/createCollection", CollectionsControllers.createCollection)
 
-//obtener el listado de una colección
+//obtener el listado de una colección, paginados y con filtros
 
 //actualizar el nombre de una colección
+router.put("/updateCollection", CollectionsControllers.updateName)
 
 //eliminar una colección
 
