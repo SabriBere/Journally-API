50efd9a82ea53b0064fa045147d2989f32f3bb11
SabriBere <sabrinab786@gmail.com>
2025-07-27 18:41:31 -0300

    secret para refresh token + función generadora

--- api/utils/jwt.ts
+++ api/utils/jwt.ts
@@ -7,6 +7,12 @@ export function generateToken(payload: object, expiresIn = 3600) {
     return jwt.sign(payload, JWT_SECRET, { expiresIn });
 }
 
+export function generateRefreshToken(payload: object, expiresIn = 36000) {
+    return jwt.sign(payload, JWT_SECRET, { expiresIn });
+}
+
+//implementar un refreshToken
+
 export function verifyToken(token: string) {
     return jwt.verify(token, JWT_SECRET);
 }
