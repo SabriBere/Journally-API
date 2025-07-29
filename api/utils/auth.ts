86a622258732964f10fda5a111363a8c21026f16
Sabrina Demetrio <sabrinab786@gmail.com>
2025-07-29 14:42:49 -0300

    ajuste del middleware para que reciba y valide headers

--- api/utils/auth.ts
+++ api/utils/auth.ts
@@ -3,16 +3,15 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET! as string;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET! as string;
 
-//3600 = 1h
-export function generateToken(payload: object, expiresIn = 3600) {
+//15m
+export function generateToken(payload: object, expiresIn = 15 * 60) {
     return jwt.sign(payload, JWT_SECRET, { expiresIn });
 }
 
-//10800 7 días
-export function generateRefreshToken(payload: object, expiresIn = 7 * 24 * 60 * 60 ) {
+//7 días
+export function generateRefreshToken(
+    payload: object,
+    expiresIn = 7 * 24 * 60 * 60
+) {
     return jwt.sign(payload, JWT_REFRESH_SECRET, { expiresIn });
-}
-
-export function verifyToken(token: string) {
-    return jwt.verify(token, JWT_SECRET);
 }
