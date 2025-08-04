3309f6570c4963f81ebe581239087493bbcde68d
SabriBere <sabrinab786@gmail.com>
2025-08-04 18:43:22 -0300

    ajustes al middleware de auth

--- api/utils/auth.ts
+++ api/utils/auth.ts
@@ -3,8 +3,8 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET! as string;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET! as string;
 
-//15m
-export function generateToken(payload: object, expiresIn = 15 * 60) {
+//15m 15 * 60 --> para desarrollo lo voy a extender
+export function generateToken(payload: object, expiresIn = 7 * 24 * 60 * 60) {
     return jwt.sign(payload, JWT_SECRET, { expiresIn });
 }
 
