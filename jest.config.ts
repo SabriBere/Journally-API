bc321e3c4702b9ebbe86a2977312bb4bc59464f8
Sabrina Demetrio <95036708+SabriBere@users.noreply.github.com>
2026-05-05 11:54:51 -0300

    agregando comandos para testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,1 +1,11 @@
-//configuración para jest
+import type { Config } from "jest";
+
+const config: Config = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: [
+        "**/?(*.)+(spec|test).ts",
+    ],
+};
+
+export default config;
